dad153602a9a049f18d120b4a0af2d6f76937ebe
Imbryk <hayos_644@wp.pl>
2019-04-21 02:33:00 +0200

    test for 2 signals registration has been added

--- embedded/trunk/notyficationLayer/tests/src/notification_reg.cpp
+++ embedded/trunk/notyficationLayer/tests/src/notification_reg.cpp
@@ -21,9 +21,13 @@ static void notify(uint8 key, void *newValue, uint16 size)
 
 static notfyUnit_t notfyUnit;
 static notification_t notification;
+static notification_t notification2;
 
 static uint64 testVariable = 0x12345678acbde988;
 static uint8 testVariableLock;
+
+static uint64 testVariable2[2] = {0x13348acbde988566,0x2233441122665544};
+static uint8 testVariableLock2;
 
 namespace
 {
@@ -46,6 +50,42 @@ namespace
     EXPECT_EQ(memcmp(notfyUnit.monUnit[0].backupValue,
               notification.pMonitorAddress,
               sizeof(notification.size) ), 0);
+  }
+
+  TEST_F(ntfyTest, two_signals)
+  {
+//    EXPECT_EQ(notification_Init(&notfyUnit, notify), TRUE);
+//    notification.key = 10;
+//    notification.lock = &testVariableLock;
+//    notification.pMonitorAddress = &testVariable;
+//    notification.size = sizeof(testVariable);
+//    EXPECT_EQ(notificaiton_Register(&notfyUnit, &notification), TRUE);
+//    EXPECT_EQ(notfyUnit.monTopPtr, 1);
+//    EXPECT_EQ(notfyUnit.monUnit[0].baseValue, notification.pMonitorAddress);
+//    EXPECT_EQ(notfyUnit.monUnit[0].size, notification.size);
+//    EXPECT_EQ(notfyUnit.monUnit[0].backupValue, notfyUnit.bkpArea);
+//    EXPECT_EQ(notfyUnit.monUnit[0].lock, notification.lock);
+//    EXPECT_EQ(notfyUnit.monUnit[0].key, notification.key);
+//    EXPECT_EQ(notfyUnit.monAreaPtr, notification.size);
+//    EXPECT_EQ(memcmp(notfyUnit.monUnit[0].backupValue,
+//              notification.pMonitorAddress,
+//              sizeof(notification.size) ), 0);
+
+    notification2.key = 11;
+    notification2.lock = &testVariableLock2;
+    notification2.pMonitorAddress = &testVariable2;
+    notification2.size = sizeof(testVariable2);
+    EXPECT_EQ(notificaiton_Register(&notfyUnit, &notification2), TRUE);
+    EXPECT_EQ(notfyUnit.monTopPtr, 2);
+    EXPECT_EQ(notfyUnit.monUnit[1].baseValue, notification2.pMonitorAddress);
+    EXPECT_EQ(notfyUnit.monUnit[1].size, notification2.size);
+    EXPECT_EQ(notfyUnit.monUnit[1].backupValue, notfyUnit.bkpArea+notification.size);
+    EXPECT_EQ(notfyUnit.monUnit[1].lock, notification2.lock);
+    EXPECT_EQ(notfyUnit.monUnit[1].key, notification2.key);
+    EXPECT_EQ(notfyUnit.monAreaPtr, notification.size + notification2.size);
+    EXPECT_EQ(memcmp(notfyUnit.monUnit[1].backupValue,
+              notification2.pMonitorAddress,
+              sizeof(notification2.size) ), 0);
   }
 }
 
